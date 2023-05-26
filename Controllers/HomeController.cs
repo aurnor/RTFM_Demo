dd37d068eec78a3d1a22ce74d5fb9ce5ccdd21ea
romain zert <romain.zert@outlook.com>
2023-05-26 10:39:23 +0200

    added call

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebApp1.Models;
+using WebApp1.Service;
 
 namespace WebApp1.Controllers;
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<HomeController> _logger;   
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+        constosoDBService = new ConstosoDBService();
     }
+
+    private readonly ConstosoDBService constosoDBService;
 
     public IActionResult Index()
     {
+
+        this.Request.Headers.TryGetValue("username", out var customerName);
+        constosoDBService.LogUserExist(customerName);
+
+
         return View();
     }
 
--- Service/ConstosoDBService.cs
+++ Service/ConstosoDBService.cs
@@ -5,7 +5,7 @@ namespace WebApp1.Service
 {
     public class ConstosoDBService
     {
-        public void ExecuteGenericRequest(string customerName)
+        public void LogUserExist(string customerName)
         {
             string connectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;";
             string commandText = "SELECT * FROM Customers WHERE CustomerName = '" + customerName + "';";
