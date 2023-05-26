8838033b4e1517d66f13c920741a0a3afc00d790
romain zert <romain.zert@outlook.com>
2023-05-26 18:24:52 +0200

    added label

--- Service/ConstosoDBService.cs
+++ Service/ConstosoDBService.cs
@@ -13,7 +13,7 @@ namespace WebApp1.Service
             // BAD: the category might have SQL special characters in it
             using (var connection = new SqlConnection(connectionString))
             {
-                var query1 = "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='"
+                var query1 = "SELECT ITEM,PRICE,LABEL FROM PRODUCT WHERE ITEM_CATEGORY='"
                   + inpString + "' ORDER BY PRICE";
                 var adapter = new SqlDataAdapter(query1, connection);
                 var result = new DataSet();
