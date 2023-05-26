da9671859f9ff66a57bc338cb9324e7ca9846fc1
romain zert <romain.zert@outlook.com>
2023-05-26 21:08:04 +0200

    sql injection

--- Service/ConstosoDBService.cs
+++ Service/ConstosoDBService.cs
@@ -11,15 +11,15 @@ namespace WebApp1.Service
             string connectionString = "Server=tcp:test-fw-sqlmi.20909H2H.database.windows.net,1433;Persist Security Info=False;User ID=your_username;Password=your_password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication=\"Active Directory Password\";";
 
             // BAD: the category might have SQL special characters in it
-            //using (var connection = new SqlConnection(connectionString))
-            //{
-            //    var query1 = "SELECT ITEM,PRICE,LABEL FROM PRODUCT WHERE ITEM_CATEGORY='"
-            //      + inpString + "' ORDER BY PRICE";
-            //    var adapter = new SqlDataAdapter(query1, connection);
-            //    var result = new DataSet();
-            //    adapter.Fill(result);
-            //    return result;
-            //}
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query1 = "SELECT ITEM,PRICE,LABEL FROM PRODUCT WHERE ITEM_CATEGORY='"
+                  + inpString + "' ORDER BY PRICE";
+                var adapter = new SqlDataAdapter(query1, connection);
+                var result = new DataSet();
+                adapter.Fill(result);
+                return result;
+            }
 
             // GOOD: use parameters with stored procedures
             using (var connection = new SqlConnection(connectionString))
